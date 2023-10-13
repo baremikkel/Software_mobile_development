fe934e5d09b40fdd7f73a737c31aa10f9c6d0a87
baremikkel <75213352+baremikkel@users.noreply.github.com>
2023-10-13 13:17:52 +0200

    added scroll menu on home page

--- car_app/App.tsx
+++ car_app/App.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text} from 'react-native';
+import Homescreen from './sites/homescreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Homescreen></Homescreen>
+    </SafeAreaView>
   );
 }
 
