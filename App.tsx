538987dc6f0786650698b9f03c607b13a81befa0
Magnus Jensen <magnushjensen.mail@gmail.com>
2023-10-18 17:54:19 +0200

    add query client and navigation stack

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text} from 'react-native';
-import Homescreen from './sites/homescreen'
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CarList } from './src/views/car-list/CarList';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Homescreen></Homescreen>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="CarList">
+            <Stack.Screen name="CarList" component={CarList} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
