0ead3a36d320aba959e33c06e2e1e01ba10610f5
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2023-11-07 09:39:10 +0100

    Updated the routing from carlist to carview by creating a new screen containing both in the app file

--- App.tsx
+++ App.tsx
@@ -1,158 +1,167 @@
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {
-  type BottomTabBarProps,
-  createBottomTabNavigator
+    type BottomTabBarProps,
+    createBottomTabNavigator
 } from '@react-navigation/bottom-tabs';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CarList } from './src/views/car-list/CarList';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {CarList} from './src/views/car-list/CarList';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {
-  AddIcon,
-  GluestackUIProvider,
-  Icon,
-  MenuIcon,
-  SettingsIcon,
-  Text,
-  View
+    AddIcon,
+    GluestackUIProvider,
+    Icon,
+    MenuIcon,
+    SettingsIcon,
+    Text,
+    View
 } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config';
-import { TouchableOpacity } from 'react-native';
-import { registerRootComponent } from 'expo';
+import {config} from '@gluestack-ui/config';
+import {TouchableOpacity} from 'react-native';
+import {registerRootComponent} from 'expo';
 import React from 'react';
-import {CarView} from "./src/views/car-view/CarView";
+import {CarView} from './src/views/car-view/CarView';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient();
 
 const App = (): JSX.Element => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <GluestackUIProvider config={config}>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              initialRouteName="CarList"
-              tabBar={(props) => TabBar({ ...props })}
-              screenOptions={{
-                headerShown: false
-              }}
-            >
-              {routes.map((route) => (
-                <Tab.Screen
-                  key={route.name}
-                  name={route.name}
-                  component={route.component}
-                />
-              ))}
-            </Tab.Navigator>
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </GluestackUIProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <GluestackUIProvider config={config}>
+                <SafeAreaProvider>
+                    <NavigationContainer>
+                        <Tab.Navigator
+                            initialRouteName="CarViewWithNavigator"
+                            tabBar={(props) => TabBar({...props})}
+                            screenOptions={{
+                                headerShown: false
+                            }}
+                        >
+                            {routes.map((route) => (
+                                <Tab.Screen
+                                    key={route.name}
+                                    name={route.name}
+                                    component={route.component}
+                                />
+                            ))}
+                        </Tab.Navigator>
+                    </NavigationContainer>
+                </SafeAreaProvider>
+            </GluestackUIProvider>
+        </QueryClientProvider>
+    );
 };
+
+const CarRouter = () => {
+    return <Stack.Navigator
+        screenOptions={{
+            headerShown: false
+        }}>
+        <Stack.Screen name="CarList" component={CarList}
+        />
+        <Stack.Screen name="CarView" component={CarView}/>
+    </Stack.Navigator>
+}
 
 type Route = {
-  name: string;
-  component: () => JSX.Element;
-  icon: any;
-  disabled: boolean;
+    name: string;
+    component: () => JSX.Element;
+    icon: any;
+    disabled: boolean;
 };
 
-// @ts-expect-error Produces a complex type error, but it works
+// @ts-expect-error Produces a complex typ
+// e error, but it works
 const routes: Route[] = [
-  {
-    name: 'AddCar',
-    component: CarList,
-    icon: AddIcon,
-    disabled: true
-  },
-  {
-    name: 'CarList',
-    component: CarList,
-    icon: MenuIcon,
-    disabled: false
-  },
-  {
-    name: 'Profile',
-    component: CarList,
-    icon: SettingsIcon,
-    disabled: true
-  },
-  {
-    name: 'CarView',
-    component: CarView,
-    icon: SettingsIcon,
-    disabled: false
-  }
+    {
+        name: 'AddCar',
+        component: CarList,
+        icon: AddIcon,
+        disabled: true
+    },
+    {
+        name: 'CarViewWithNavigator',
+        component: CarRouter,
+        icon: MenuIcon,
+        disabled: false
+    },
+    {
+        name: 'Profile',
+        component: CarList,
+        icon: SettingsIcon,
+        disabled: true
+    }
 ];
 
+
 function TabBar({
-  state,
-  descriptors,
-  navigation
-}: BottomTabBarProps): JSX.Element {
-  return (
-    <View
-      display="flex"
-      flexDirection="row"
-      paddingHorizontal="$8"
-      paddingVertical="$4"
-      justifyContent="space-between"
-      width="$full"
-    >
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const matchedRoute = routes.find((r) => r.name === route.name);
-        const icon = matchedRoute?.icon ? (
-          <Icon
-            as={matchedRoute.icon}
-            size="xl"
-            color={matchedRoute.disabled ? '$backgroundDark200' : '$black'}
-          />
-        ) : (
-          <Text>?</Text>
-        );
+                    state,
+                    descriptors,
+                    navigation
+                }: BottomTabBarProps): JSX.Element {
+    return (
+        <View
+            display="flex"
+            flexDirection="row"
+            paddingHorizontal="$8"
+            paddingVertical="$4"
+            justifyContent="space-between"
+            width="$full"
+        >
+            {state.routes.map((route, index) => {
+                const {options} = descriptors[route.key];
+                const matchedRoute = routes.find((r) => r.name === route.name);
+                const icon = matchedRoute?.icon ? (
+                    <Icon
+                        as={matchedRoute.icon}
+                        size="xl"
+                        color={matchedRoute.disabled ? '$backgroundDark200' : '$black'}
+                    />
+                ) : (
+                    <Text>?</Text>
+                );
 
-        const isFocused = state.index === index;
+                const isFocused = state.index === index;
 
-        const onPress = (): void => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true
-          });
+                const onPress = (): void => {
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true
+                    });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+                    if (!isFocused && !event.defaultPrevented) {
+                        navigation.navigate(route.name);
+                    }
+                };
 
-        const onLongPress = (): void => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key
-          });
-        };
+                const onLongPress = (): void => {
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key
+                    });
+                };
 
-        // TODO: Add background highlight if the tab is focused
-        return (
-          <TouchableOpacity
-            key={index}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            disabled={matchedRoute?.disabled}
-          >
-            {icon}
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
+                // TODO: Add background highlight if the tab is focused
+                return (
+                    <TouchableOpacity
+                        key={index}
+                        accessibilityRole="button"
+                        accessibilityState={isFocused ? {selected: true} : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        disabled={matchedRoute?.disabled}
+                    >
+                        {icon}
+                    </TouchableOpacity>
+                );
+            })}
+        </View>
+    );
 }
 
 export default App;
