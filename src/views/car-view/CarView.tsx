14f75bd0625941647cc9c46db832ed728cf08d1b
Pizzafuglen <Crose19@student.sdu.dk>
2023-11-11 17:37:24 +0100

    Implemented the CarPictureScroll component using the car prop

--- src/views/car-view/CarView.tsx
+++ src/views/car-view/CarView.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {CarPictureScroll} from "./components/CarPictureScroll";
-import {View,Text} from "@gluestack-ui/themed";
-export const CarView = () => {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+import {CarPictureScroll} from './components/CarPictureScroll';
+import {View, Text} from '@gluestack-ui/themed';
+
+export const CarView = ({route}): React.JSX.Element => {
+    return (<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+
+            <CarPictureScroll curCar={route.params.car}/>
             <Text>
                 This is the *** car view
             </Text>
-            <CarPictureScroll>
-
-            </CarPictureScroll>
-        </View>
-    );
+        </View>);
 }
