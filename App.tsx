5398d91e984e65e2778de7bf815d14a444d4e330
CHRosenkilde <90039392+Pizzafuglen@users.noreply.github.com>
2023-12-10 16:43:01 +0100

    Car view (#4)
    * Work done on the individual car detail
    
    * Updated the routing from carlist to carview by creating a new screen containing both in the app file
    
    * Added button for routing to the carview from the carlist (carcard instance contains button)
    
    * Minor changes
    
    * Minor changes
    
    * Minor changes adding in the carpicturescroll into the carview object
    
    * Indentation changes to the App.tsx file
    
    * Corrected use of navigation to the CarView.tsx using the GoToCarView method
    
    * Minor indentation adjustments
    
    * Removed old use of FlatList changing it to a horizontal ScrollView instead allowing for more pictures to be loaded in (added a TODO)
    
    * Moved the TODO
    
    * Implemented the CarPictureScroll component using the car prop
    
    * Updated firebase
    
    * Revert "Updated firebase"
    
    This reverts commit 85e96261b74a450dad412ff45cb3a4e8dc24a16d.
    
    * Revert "Implemented the CarPictureScroll component using the car prop"
    
    This reverts commit 14f75bd0625941647cc9c46db832ed728cf08d1b.
    
    * Car view is done

--- App.tsx
+++ App.tsx
@@ -19,37 +19,52 @@ import { config } from '@gluestack-ui/config';
 import { TouchableOpacity } from 'react-native';
 import { registerRootComponent } from 'expo';
 import React from 'react';
+import { CarView } from './src/views/car-view/CarView';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient();
 
 const App = (): JSX.Element => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <GluestackUIProvider config={config}>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              initialRouteName="CarList"
-              tabBar={(props) => TabBar({ ...props })}
-              screenOptions={{
-                headerShown: false
-              }}
-            >
-              {routes.map((route) => (
-                <Tab.Screen
-                  key={route.name}
-                  name={route.name}
-                  component={route.component}
-                />
-              ))}
-            </Tab.Navigator>
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </GluestackUIProvider>
-    </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+            <GluestackUIProvider config={config}>
+                <SafeAreaProvider>
+                    <NavigationContainer>
+                        <Tab.Navigator
+                            initialRouteName="CarViewWithNavigator"
+                            tabBar={(props) => TabBar({ ...props })}
+                            screenOptions={{
+                              headerShown: false
+                            }}
+                        >
+                            {routes.map((route) => (
+                                <Tab.Screen
+                                    key={route.name}
+                                    name={route.name}
+                                    component={route.component}
+                                />
+                            ))}
+                        </Tab.Navigator>
+                    </NavigationContainer>
+                </SafeAreaProvider>
+            </GluestackUIProvider>
+        </QueryClientProvider>
   );
 };
+
+const CarRouter = () => {
+  return <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name="CarList" component={CarList}
+        />
+        <Stack.Screen name="CarView" component={CarView}
+        />
+    </Stack.Navigator>
+}
 
 type Route = {
   name: string;
@@ -58,7 +73,8 @@ type Route = {
   disabled: boolean;
 };
 
-// @ts-expect-error Produces a complex type error, but it works
+// @ts-expect-error Produces a complex typ
+// e error, but it works
 const routes: Route[] = [
   {
     name: 'AddCar',
@@ -67,8 +83,8 @@ const routes: Route[] = [
     disabled: true
   },
   {
-    name: 'CarList',
-    component: CarList,
+    name: 'CarViewWithNavigator',
+    component: CarRouter,
     icon: MenuIcon,
     disabled: false
   },
@@ -86,65 +102,65 @@ function TabBar({
   navigation
 }: BottomTabBarProps): JSX.Element {
   return (
-    <View
-      display="flex"
-      flexDirection="row"
-      paddingHorizontal="$8"
-      paddingVertical="$4"
-      justifyContent="space-between"
-      width="$full"
-    >
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const matchedRoute = routes.find((r) => r.name === route.name);
-        const icon = matchedRoute?.icon ? (
-          <Icon
-            as={matchedRoute.icon}
-            size="xl"
-            color={matchedRoute.disabled ? '$backgroundDark200' : '$black'}
-          />
-        ) : (
-          <Text>?</Text>
-        );
+        <View
+            display="flex"
+            flexDirection="row"
+            paddingHorizontal="$8"
+            paddingVertical="$4"
+            justifyContent="space-between"
+            width="$full"
+        >
+            {state.routes.map((route, index) => {
+              const { options } = descriptors[route.key];
+              const matchedRoute = routes.find((r) => r.name === route.name);
+              const icon = matchedRoute?.icon ? (
+                    <Icon
+                        as={matchedRoute.icon}
+                        size="xl"
+                        color={matchedRoute.disabled ? '$backgroundDark200' : '$black'}
+                    />
+              ) : (
+                    <Text>?</Text>
+              );
 
-        const isFocused = state.index === index;
+              const isFocused = state.index === index;
 
-        const onPress = (): void => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true
-          });
+              const onPress = (): void => {
+                const event = navigation.emit({
+                  type: 'tabPress',
+                  target: route.key,
+                  canPreventDefault: true
+                });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+                if (!isFocused && !event.defaultPrevented) {
+                  navigation.navigate(route.name);
+                }
+              };
 
-        const onLongPress = (): void => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key
-          });
-        };
+              const onLongPress = (): void => {
+                navigation.emit({
+                  type: 'tabLongPress',
+                  target: route.key
+                });
+              };
 
-        // TODO: Add background highlight if the tab is focused
-        return (
-          <TouchableOpacity
-            key={index}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            disabled={matchedRoute?.disabled}
-          >
-            {icon}
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+              // TODO: Add background highlight if the tab is focused
+              return (
+                    <TouchableOpacity
+                        key={index}
+                        accessibilityRole="button"
+                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        disabled={matchedRoute?.disabled}
+                    >
+                        {icon}
+                    </TouchableOpacity>
+              );
+            })}
+        </View>
   );
 }
 
